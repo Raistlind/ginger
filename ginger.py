d2b8e1b03cc13fa2d8096743e63ad451b8616ec6
RaistlinD <Johnd0712@gmail.com>
2019-04-06 23:41:30 +0800

    init

---
+++ ginger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   ginger.py    
+@Contact :   Johnd0712@hotmail.com
+@License :   (C)Copyright 2017-2018, Krynn.cn
+
+@Modify Time         @Author      @Version    @Description
+-----------------    ---------    --------    -----------
+2019-04-06 23:38      RaistlinD    1.0         None
+"""
+
+# import lib
+
