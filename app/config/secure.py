21dc71d3ad5f2c8682b7eb4012ad431411a1b3d8
RaistlinD <Johnd0712@gmail.com>
2019-04-07 20:24:50 +0800

    init

--- app/config/secure.py
+++ app/config/secure.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-@File    :   app.py    
+@File    :   secure.py    
 @Contact :   Johnd0712@hotmail.com
 @License :   (C)Copyright 2017-2018, Krynn.cn
 
 @Modify Time         @Author      @Version    @Description
 -----------------    ---------    --------    -----------
-2019-04-06 23:39      RaistlinD    1.0         None
+2019-04-07 20:07      RaistlinD    1.0         None
 """
 
 # import lib
-from flask import Flask
 
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('app.config.setting')
-    app.config.from_object('app.config.secure')
-    return app
--- app/config/setting.py
+++ app/config/setting.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-@File    :   __init__.py.py    
+@File    :   setting.py    
 @Contact :   Johnd0712@hotmail.com
 @License :   (C)Copyright 2017-2018, Krynn.cn
 
 @Modify Time         @Author      @Version    @Description
 -----------------    ---------    --------    -----------
-2019-04-06 23:39      RaistlinD    1.0         None
+2019-04-07 20:07      RaistlinD    1.0         None
 """
 
 # import lib
